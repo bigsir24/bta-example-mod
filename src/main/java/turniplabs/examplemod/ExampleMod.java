96bd8cbee8a11abd5b46f459fa24fdae0fe2f459
bigsir24 <169484568+bigsir24@users.noreply.github.com>
2026-06-09 15:24:13 +0200

    Mod version now includes the BTA version as metadata, readded input.properties, updated ExampleMod

--- src/main/java/turniplabs/examplemod/ExampleMod.java
+++ src/main/java/turniplabs/examplemod/ExampleMod.java
@@ -4,35 +4,19 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.HalpLibe;
-import turniplabs.halplibe.util.GameStartEntrypoint;
-import turniplabs.halplibe.util.RecipeEntrypoint;
+import turniplabs.halplibe.event.defs.CommonEvents;
 
-public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+public class ExampleMod implements ModInitializer {
 	public static final String MOD_ID = HalpLibe.registerMod("examplemod", true);
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("ExampleMod initialized.");
+		CommonEvents.BEFORE_GAME_START.listen(ExampleMod::beforeGameStart);
 	}
 
-	@Override
-	public void beforeGameStart() {
-
-	}
-
-	@Override
-	public void afterGameStart() {
-
-	}
-
-	@Override
-	public void onRecipesReady() {
-
-	}
-
-	@Override
-	public void initNamespaces() {
-
+	private static void beforeGameStart() {
+		LOGGER.info("This is an example event!");
 	}
 }
